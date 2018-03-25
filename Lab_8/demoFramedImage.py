49c30990462266c1185c12ec2825b38fe483bff2
kronokiller <kronokiller01@yahoo.com>
2018-03-24 19:32:10 -0500

    Update demoFramedImage.py

--- Lab_8/demoFramedImage.py
+++ Lab_8/demoFramedImage.py
@@ -3,12 +3,12 @@ Demonstrates the displayFramedImage function
 """
 
 import lab8Functions
-import cImage
+import scipy.misc as misc
 
 # Frame a photo with a narrow yellow frame
-myImage = cImage.FileImage("flowers.png")
+myImage = misc.imread("flowers.png")
 lab8Functions.displayFramedImage(myImage, 20, lab8Functions.yellowPixel)
 
 # Frame some artwork with a magenta frame
-tugImage = cImage.FileImage("tug.png")
+tugImage = misc.imread("tug.png")
 lab8Functions.displayFramedImage(tugImage, 50, lab8Functions.magentaPixel)
